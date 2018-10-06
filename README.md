ceac1c2dbe09f7f3108c6a5d44c366a569a47638
Hasitha Bhagya Seneviratne <hasitha.bigwig@gmail.com>
2018-10-06 20:23:45 +0530

    Code refactoring | Update Readme

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ After you installed packages, these commands are available in `package.json`.
 npm run dev # run in development mode
 ```
 Navigate to ``` http://localhost:3000/ ```. The app will automatically reload if you change any of the source files.
+
+
+## Deploy
+
+Change ```cookie: { secure: true, httpOnly: true }``` when deploying to Production environment
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const session = require('express-session')
 const bodyParser = require('body-parser');
-const csurf = require('csurf');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const morgan = require('morgan')
@@ -14,35 +13,21 @@ app.set('views', path.join(__dirname, '/public'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public/assets'));
 
-// const csrfMiddleware = csurf({
-//   sessionKey: 'sessionID',
-//   cookie: true,
-//   ignoreMethods: ['GET', 'HEAD', 'OPTIONS']
-// });
-
 app.use(session({
   name: 'sessionID',
   secret: 'SLIITSSDKt2HA454tYPW',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false }
+  cookie: { secure: false, httpOnly: true }
 }))
+// Change cookie: { secure: true, httpOnly: true } when deploying to Production environment
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
 app.use(morgan('dev'))
 app.use(cookieParser());
-// app.use(csrfMiddleware);
-
-// error handler
-// app.use(function (err, req, res, next) {
-//   if (err.code !== 'EBADCSRFTOKEN') return next(err)
-
-//   // handle CSRF token errors here
-//   res.status(403)
-//   res.send('Invalid CSRF found!')
-// })
 
 // Views
 app.get('/', function(req, res) {
@@ -70,19 +55,19 @@ app.post('/login', function (req, res) {
 })
 
 app.post('/formsubmit', function(req, res) {
-  let msgTxt = '';
-  let reason = '';
-  let className = '';
   if (req.session.csrf == req.body._csrf) {
-    msgTxt = 'Your contact information has been successfully added!';
-    reason = 'CSRF token is valid!';
-    className = 'success';
+    res.render('views/message', {
+      msgTxt: 'Your contact information has been successfully added!',
+      reason: 'CSRF token is valid!',
+      className: 'success'
+    });
   } else {
-    msgTxt = 'Your contact information is invalid.';
-    reason = 'Valid CSRF token required!';
-    className = 'fail';
+    res.render('views/message', {
+      msgTxt: 'Your contact information is invalid.',
+      reason: 'Valid CSRF token required!',
+      className: 'fail'
+    });
   }
-  res.render('views/message', { msgTxt: msgTxt, reason: reason, className: className });
 })
 
 
